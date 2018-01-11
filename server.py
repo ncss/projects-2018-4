74a8a76df9303e31010bda15875d53a4b9a40e98
LiamWA11 <angusliam5@gmail.com>
2018-01-11 16:58:03 +1100

    Added a server.py

---
+++ server.py
@@ -0,0 +1,65 @@
+from tornado.ncss import Server, ncssbook_log
+
+html = '''
+<html>
+
+	<body>
+	
+		<form method="POST" emctype="multipart/form-data">
+			<input type="text" name="torchic"><br>
+			<input type="file" name="photo"><br>
+			<input type="submit">
+		</form>
+	
+	</body>
+
+</html>
+'''
+
+content_type = ''
+filename = ''
+photo = ''
+
+def index_handler(response):
+    global filename, content_type, photo
+    torchic = response.get_field('torchic')
+    filename, content_type, photo = response.get_file('photo')
+    if torchic == None:
+        response.write(html)
+    else:
+        response.write(torchic)
+        print(photo)
+        response.write('<img src="/photo" alt="{}">'.format(filename))
+
+    
+    
+
+def upload_handler(response):
+    response.write('Upload Page')
+
+def profile_handler(response, user):
+    if user.lower() == 'liam':
+        response.write("LIAM IS AWESOMEEEEE")
+    else:
+        response.write('This is the profile page of: ' + str(user))
+
+#------------------
+def photo(response):
+    global content_type, photo
+    print(content_type)
+    print(photo)
+    response.set_header('Content-Type', 'image/png')
+    response.write(photo)
+
+
+server = Server()
+
+server.register('/', index_handler)
+server.register('/upload', upload_handler)
+server.register(r'/profile/(.+)', profile_handler)
+
+#---------------
+server.register('/photo', photo)
+
+if __name__ == "__main__":
+    server.run()
