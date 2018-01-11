20f441fe87a07ff88599a802e2c0c011f92158a5
selsworth <sels8503@SHARED.SYDNEY.EDU.AU>
2018-01-11 16:03:40 +1100

    added backend

---
+++ tornado_clone.py
@@ -0,0 +1,17 @@
+from tornado.ncss import Server, ncssbook_log
+
+def index_handler(response):
+	response.write('hello')
+	
+def upload(response):
+	response.write('this is the upload page :)')
+	
+def profile(response):
+	response.write('this is the profile page :)')
+	
+server = Server()
+server.register(r'/', index_handler)
+server.register(r'/upload', upload)
+server.register(r'/profile', profile)
+
+server.run()
