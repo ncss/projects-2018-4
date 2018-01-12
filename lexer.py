9b67d4b0967e8cdf30bdd641996342f4c7e388ec
Owen Chandler <ochandle607@gmail.com>
2018-01-12 15:51:52 +1100

    Made same-line for and if blocks work properly

--- lexer.py
+++ lexer.py
@@ -26,10 +26,10 @@ special_characters_dict = {
     }
 
 RE_FIND_BRACES = re.compile(r'({{)|(}})|({%)|(%})', re.MULTILINE)
-RE_FIND_IF = re.compile(r'\s+if\s+(.+?)\s+%}', re.MULTILINE)
-RE_FIND_END_IF = re.compile(r'\s+end if\s+?%}', re.MULTILINE)
-RE_FIND_FOR = re.compile(r'\sfor\s(\w+?)\sin\s(\w+?)\s%}')
-RE_FIND_END_FOR = re.compile(r'\s+end for\s+?%}')
+RE_FIND_IF = re.compile(r'{%\s+if\s+(.+?)\s+%}', re.MULTILINE)
+RE_FIND_END_IF = re.compile(r'{%\s+end if\s+?%}', re.MULTILINE)
+RE_FIND_FOR = re.compile(r'{%\sfor\s(\w+?)\sin\s(\w+?)\s%}')
+RE_FIND_END_FOR = re.compile(r'{%\s+end for\s+?%}')
 
 def tokenise(text: str) -> list:
 
@@ -56,29 +56,30 @@ def tokenise(text: str) -> list:
             end_if = RE_FIND_END_IF.search(text[end:])
             start_for = RE_FIND_FOR.search(text[end:])
             end_for = RE_FIND_END_FOR.search(text[end:])
-            if start_if:
+
+            matches = [start_if, end_if, start_for, end_for]
+
+            code = sorted([i for i in matches if i], key=lambda x: x.start())[0]
+            if code == start_if:
                 tokens.append((IF, "if"))
                 tokens.append((TEXT, start_if.group(1)))
                 tokens.append((CLOSE_PERCENT, "%}"))
                 start = start_if.end() + end
-            else:
-                if end_if:
-                    tokens.append((END_IF, "end if"))
-                    tokens.append((CLOSE_PERCENT, "%}"))
-                    start = end_if.end() + end
-                else:
-                    if start_for:
-                        tokens.append((FOR, "for"))
-                        tokens.append((TEXT, start_for.group(1)))
-                        tokens.append((IN, "in"))
-                        tokens.append((ITERABLE, start_for.group(2)))
-                        tokens.append((CLOSE_PERCENT, "%}"))
-                        start = start_for.end() + end
-                    else:
-                        if end_for:
-                            tokens.append((END_FOR, "end for"))
-                            tokens.append((CLOSE_PERCENT, "%}"))
-                            start = end_for.end() + end
+            elif code == end_if:
+                tokens.append((END_IF, "end if"))
+                tokens.append((CLOSE_PERCENT, "%}"))
+                start = end_if.end() + end
+            elif code == start_for:
+                tokens.append((FOR, "for"))
+                tokens.append((TEXT, start_for.group(1)))
+                tokens.append((IN, "in"))
+                tokens.append((ITERABLE, start_for.group(2)))
+                tokens.append((CLOSE_PERCENT, "%}"))
+                start = start_for.end() + end
+            elif code == end_for:
+                tokens.append((END_FOR, "end for"))
+                tokens.append((CLOSE_PERCENT, "%}"))
+                start = end_for.end() + end
         else: start = match.end()
     tokens.append((TEXT, text))
 
@@ -88,8 +89,24 @@ def lex(text):
     return tokenise(text)
 
 if __name__ == "__main__":
-    with open("test_data.txt") as f:
-        result_file = open("result.txt", "w")
-        for line in f:
-            print(tokenise(line), file=result_file)
-        result_file.close()
+    fails = []
+    def check_equal(input, expected_tokens):
+        global fails
+        output = lex(input)
+        if output != expected_tokens:
+            fails += [(output, expected_tokens)]
+
+    check_equal('a', [(TEXT, 'a')])
+    check_equal('a{{ name }}b', [(TEXT, 'a'), (OPEN_BRACE, "{{"), (TEXT, " name "), (CLOSE_BRACE, "}}"), (TEXT, "b")])
+    check_equal('{% if x %} do stuff with {{ value }} {% end if %}',
+    [(OPEN_PERCENT, "{%"), (IF, 'if'), (TEXT, "x"), (CLOSE_PERCENT, "%}"), (TEXT, " do stuff with "), (OPEN_BRACE, "{{"), (TEXT, " value "), (CLOSE_BRACE, "}}"), (TEXT, " "), (OPEN_PERCENT, "{%"), (END_IF, "end if"), (CLOSE_PERCENT, "%}"), (TEXT, "")])
+    check_equal('{% if x %}{% for y in z %}', [(OPEN_PERCENT, "{%"), (IF, "if"), (TEXT, "x"), (CLOSE_PERCENT, "%}"), (OPEN_PERCENT, "{%"), (FOR, "for"), (TEXT, "y"), (IN, "in"), (ITERABLE, "z"), (CLOSE_PERCENT, "%}"), (TEXT, "")])
+    if fails:
+        print(len(fails), 'Tests failed')
+        for i, (output, expected) in enumerate(fails):
+            print("Fail 1:")
+            print("Output was:", output)
+            print("Expected:", expected)
+            print()
+    else:
+        print("All tests passed!")
--- template.py
+++ template.py
@@ -5,3 +5,10 @@ def render(string, context):
     tokens = lex(string)
     tree = parse_tokens(tokens)
     return tree.returnText(context)
+
+if __name__ == '__main__':
+    output = render('<p>{{ name }}</p>', {'name': 'Pikachu'})
+    if output == '<p>Pikachu</p>':
+        print("Render works!")
+    else:
+        print("Render broke! It output", output)
