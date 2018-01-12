f3c1bd6abf6b2585a95fae5072f0b65f69926ecd
Owen Chandler <ochandle607@gmail.com>
2018-01-12 12:23:25 +1100

    Updated lexer to include for loops. It should all work as long as nested if and for loops are not on the same line.

--- lexer.py
+++ lexer.py
@@ -1,5 +1,3 @@
-# This file exports the lex function, which takes a string and returns a list of tokens.
-
 import re
 
 
@@ -10,6 +8,10 @@ OPEN_PERCENT = "{%"
 CLOSE_PERCENT = "%}"
 IF = "if"
 END_IF = "end if"
+FOR = "for"
+IN = "in"
+END_FOR = "end for"
+ITERABLE = "iterable"
 
 special_characters_dict = {
     "{{": OPEN_BRACE,
@@ -17,12 +19,17 @@ special_characters_dict = {
     "{%": OPEN_PERCENT,
     "%}": CLOSE_PERCENT,
     "if": IF,
-    "end if": END_IF
+    "end if": END_IF,
+    "for": FOR,
+    "in": IN,
+    "end for": END_FOR
     }
 
 RE_FIND_BRACES = re.compile(r'({{)|(}})|({%)|(%})', re.MULTILINE)
 RE_FIND_IF = re.compile(r'\s+if\s+(.+?)\s+%}', re.MULTILINE)
 RE_FIND_END_IF = re.compile(r'\s+end if\s+?%}', re.MULTILINE)
+RE_FIND_FOR = re.compile(r'\sfor\s(\w+?)\sin\s(\w+?)\s%}')
+RE_FIND_END_FOR = re.compile(r'\s+end for\s+?%}')
 
 def tokenise(text: str) -> list:
 
@@ -45,20 +52,33 @@ def tokenise(text: str) -> list:
         tokens.append((special_characters_dict[special_character], special_character))
 
         if special_character == "{%":
-            conditional_match = RE_FIND_IF.search(text[end:])
-            if conditional_match:
+            start_if = RE_FIND_IF.search(text[end:])
+            end_if = RE_FIND_END_IF.search(text[end:])
+            start_for = RE_FIND_FOR.search(text[end:])
+            end_for = RE_FIND_END_FOR.search(text[end:])
+            if start_if:
                 tokens.append((IF, "if"))
-                tokens.append((TEXT, conditional_match.group(1)))
+                tokens.append((TEXT, start_if.group(1)))
                 tokens.append((CLOSE_PERCENT, "%}"))
-                start = conditional_match.end()
+                start = start_if.end() + end
             else:
-                end_conditional = RE_FIND_END_IF.search(text[end:])
-                if end_conditional:
+                if end_if:
                     tokens.append((END_IF, "end if"))
                     tokens.append((CLOSE_PERCENT, "%}"))
-                    start = end_conditional.end() + end
+                    start = end_if.end() + end
                 else:
-                    raise SyntaxError("The if block must contain `if` and then a condition")
+                    if start_for:
+                        tokens.append((FOR, "for"))
+                        tokens.append((TEXT, start_for.group(1)))
+                        tokens.append((IN, "in"))
+                        tokens.append((ITERABLE, start_for.group(2)))
+                        tokens.append((CLOSE_PERCENT, "%}"))
+                        start = start_for.end() + end
+                    else:
+                        if end_for:
+                            tokens.append((END_FOR, "end for"))
+                            tokens.append((CLOSE_PERCENT, "%}"))
+                            start = end_for.end() + end
         else: start = match.end()
     tokens.append((TEXT, text))
 
