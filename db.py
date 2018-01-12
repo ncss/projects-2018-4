15b2098f44f0b21299e333aa3a0e57a1b8edb264
enthusiasticeva <evajay123@gmail.com>
2018-01-12 17:40:52 +1100

    more bug fixing

--- db.py
+++ db.py
@@ -9,12 +9,11 @@ class Category:
         
     @staticmethod
     def get_categories():
-        conn = sqlite3.connect('main.db')
+        conn = sqlite3.connect('db/main.db')
         cur = conn.cursor()
         cur.execute('''
         select *
-        from category
-        where category is not null
+        from Category;
         ''')
         categories = []
         for row in cur:
@@ -24,12 +23,12 @@ class Category:
 
     @staticmethod
     def get_category_by_id(ID):
-        conn = sqlite3.connect('main.db')
+        conn = sqlite3.connect('db/main.db')
         cur = conn.cursor()
         cur.execute('''
         select *
         from category
-        where id = ?
+        where id = ?;
         ''', (ID,))
         for row in cur:
             cur.close()
@@ -39,7 +38,7 @@ class Category:
     
     @staticmethod
     def create_category(self,image, name , information):
-        conn = sqlite3.connect('main.db')
+        conn = sqlite3.connect('db/main.db')
         cur = conn.cursor()
         allcat = get_categories(conn)
         maxid = 0
@@ -54,7 +53,7 @@ class Category:
 
 
 class Meme:
-    _conn = sqlite3.connect('main.db')
+    _conn = sqlite3.connect('db/main.db')
     
     def __init__(self, ID = None, image = None, caption = None, latitude = None, longitude = None, username = None, timestamp = None, catid = None):
         self.ID = ID
@@ -66,8 +65,10 @@ class Meme:
         self.timestamp = timestamp
         self.catid = catid
 
-    def get_memes_for_category(self,catid):
-        cur = _conn.cursor()
+    @staticmethod
+    def get_memes_for_category(catid):
+        conn = sqlite3.connect('db/main.db')
+        cur = conn.cursor()
         cur.execute('''
         select *
         from memes m
@@ -78,9 +79,11 @@ class Meme:
             memesofcat.append(Meme(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
         cur.close()
         return memesofcat
-
-    def create_meme_post(self,image, caption, latitude, longitude, unsername, timestamp, catid):
-        cur = _conn.cursor()
+    
+    @staticmethod
+    def create_meme_post(image, caption, latitude, longitude, unsername, timestamp, catid):
+        conn = sqlite3.connect('db/main.db')
+        cur = conn.cursor()
         allmemes = get_memes_for_category(conn)
         maxid = 0
         for meme in allmemes:
