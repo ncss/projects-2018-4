649d1152ff9a0fc0ebede695a84f2b09fd1489e4
enthusiasticeva <evajay123@gmail.com>
2018-01-12 15:45:40 +1100

    error fixing

--- db/create_database.py
+++ db/create_database.py
@@ -2,28 +2,8 @@ import sqlite3
 
 conn = sqlite3.connect('main.db')
 cur = conn.cursor()
-cur.execute('''CREATE TABLE category (
-  id INTEGER NOT NULL,
-  image BLOB NOT NULL,
-  information TEXT,
-  name TEXT NOT NULL,
-  PRIMARY KEY (id)
-);''')
 
-cur.execute('''
-CREATE TABLE memes (
-  id INTEGER NOT NULL,
-  image BLOG NOT NULL,
-  caption TEXT,
-  location TEXT,
-  username TEXT NOT NULL,
-  timestamp TEXT,
-  catid INTEGER NOT NULL,
-  FOREIGN KEY (catid) REFERENCES category(id),
-  PRIMARY KEY (id)
-);
-''')
 f = open('init.sql', 'r')
 for line in f:
-    if line.startswith('INSERT INTO'):
-        cur.execute(line)
+    cur.execute(line)
+conn.close()
