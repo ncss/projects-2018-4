9887350a4698dbed8d3fe50b49b8a003fc268fed
enthusiasticeva <evajay123@gmail.com>
2018-01-12 17:25:29 +1100

    database debugging

--- db.py
+++ db.py
@@ -1,14 +1,15 @@
 import sqlite3
-conn = sqlite3.connect('main.db')
-cur = conn.cursor()
-class Category:
-    def __init__(self, ID, image, information, name):
+
+class Category:    
+    def __init__(self, ID = None, image = None, information = None, name = None):
         self.ID = ID
         self.image = image
         self.information = information
         self.name = name
-
-    def get_categories(conn):
+        
+    @staticmethod
+    def get_categories():
+        conn = sqlite3.connect('main.db')
         cur = conn.cursor()
         cur.execute('''
         select *
@@ -18,9 +19,12 @@ class Category:
         categories = []
         for row in cur:
             categories.append(Category(row[0], row[1], row[2], row[3]))
+        cur.close()
         return categories
-            
-    def get_category_by_id(conn, ID):
+
+    @staticmethod
+    def get_category_by_id(ID):
+        conn = sqlite3.connect('main.db')
         cur = conn.cursor()
         cur.execute('''
         select *
@@ -28,10 +32,14 @@ class Category:
         where id = ?
         ''', (ID,))
         for row in cur:
+            cur.close()
             return Category(row[0], row[1], row[2], row[3])
+        cur.close()
         return None
-
-    def create_category(conn, image, name , information):
+    
+    @staticmethod
+    def create_category(self,image, name , information):
+        conn = sqlite3.connect('main.db')
         cur = conn.cursor()
         allcat = get_categories(conn)
         maxid = 0
@@ -42,20 +50,24 @@ class Category:
         cur.execute('''
         INSERT INTO category VALUES (?,?,?,?)
         '''(maxid,image,information,name))
+        cur.close()
 
 
-class Memes:
-    def __init__(self, ID, image, caption, location, username, timestamp, catid):
+class Meme:
+    _conn = sqlite3.connect('main.db')
+    
+    def __init__(self, ID = None, image = None, caption = None, latitude = None, longitude = None, username = None, timestamp = None, catid = None):
         self.ID = ID
         self.image = image
         self.caption = caption
-        self.location = location
+        self.latitude = latitude
+        self.longitude = longitude
         self.username = username
         self.timestamp = timestamp
         self.catid = catid
 
-    def get_memes_for_category(conn, catid):
-        cur = conn.cursor()
+    def get_memes_for_category(self,catid):
+        cur = _conn.cursor()
         cur.execute('''
         select *
         from memes m
@@ -63,18 +75,19 @@ class Memes:
         '''(catid,))
         memesofcat = []
         for row in cur:
-            memesofcat.append(Memes(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+            memesofcat.append(Meme(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
+        cur.close()
         return memesofcat
 
-    def create_meme_post(conn, image, caption, location, unsername, timestamp, catid):
-        cur = conn.cursor()
+    def create_meme_post(self,image, caption, latitude, longitude, unsername, timestamp, catid):
+        cur = _conn.cursor()
         allmemes = get_memes_for_category(conn)
         maxid = 0
         for meme in allmemes:
-            if Memes.ID > maxid:
-                maxid = Memes.ID
+            if Meme.ID > maxid:
+                maxid = Meme.ID
         maxid += 1
         cur.execute('''
-        INSERT INTO category VALUES (?,?,?,?,?,?,?)
-        '''(maxid,image,caption,location,username,timestamp,catid))
-        
+        INSERT INTO category VALUES (?,?,?,?,?,?,?,?)
+        '''(maxid,image,caption,latitude, longitude,username,timestamp,catid))
+        cur.close()
